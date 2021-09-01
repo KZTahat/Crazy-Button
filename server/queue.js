22ce996622992c6f88dd1e51d3cbca29d7927a66
laith-hayajneh <farfesh96@gmail.com>
2021-09-01 16:34:34 +0300

    mshakel

--- server/queue.js
+++ server/queue.js
@@ -4,10 +4,30 @@ const ioClient = require("socket.io-client");
 const playerNamespace = ioClient.connect(`http://localhost:3004/player`);
 
 const queue = {
-    players:{}
+    players: {}
 }
 
 playerNamespace.on('addPlayer', (playerID) => {
-    // queue.players[playerID]
-    // :)
+    queue.players[playerID] = {
+        score: 0,
+    }
+
+    console.log('score', queue.players[playerID].score);
+    console.log(playerID, 'llllll');
+})
+
+playerNamespace.on('updateScore', playerID => {
+    console.log(playerID, 'plaaaaa')
+    queue.players[playerID].score++;
+
+    console.log(queue.players[playerID].score, 'scoreeeee');
+    
+    console.log(queue);
+
+    playerNamespace.emit('newScore',queue)
+        
+ 
+
+
+
 })
