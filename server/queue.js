a766d70c56dee76693c654d43a741bc29fe32888
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2021-09-01 23:26:11 +0300

    update

--- server/queue.js
+++ server/queue.js
@@ -1,13 +1,37 @@
-"use strict";
-
 const ioClient = require("socket.io-client");
-const playerNamespace = ioClient.connect(`http://localhost:3004/player`);
+
+// for local use
+// const playerNamespace = ioClient.connect(`http://localhost:3004/player`);
+// for deployed version
+const playerNamespace = ioClient.connect(`https://crazy-button.herokuapp.com/player`);
 
 const queue = {
-    players:{}
+    players: {}
 }
 
 playerNamespace.on('addPlayer', (playerID) => {
-    // queue.players[playerID]
-    // :)
+    queue.players[playerID] = {
+        score: 0,
+    }
+
+    console.log('score', queue.players[playerID].score);
+    console.log(playerID, 'llllll');
+    playerNamespace.emit('newScore',queue);
 })
+
+playerNamespace.on('updateScore', playerID => {
+    console.log(playerID, 'plaaaaa')
+    queue.players[playerID].score++;
+
+    console.log(queue.players[playerID].score, 'scoreeeee');
+    
+    console.log(queue);
+
+    
+    
+})
+
+playerNamespace.on('overWrite',()=>{
+    console.log('overWrite on queue')
+    playerNamespace.emit('over2',queue)
+});
--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ const http = require("http");
 const express = require("express");
 const socket_io = require("socket.io");
 
+const uuid = require('uuid').v4;// random uuid
+
+
+
+
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3004;
 
@@ -13,6 +18,8 @@ let server = http.createServer(app);
 let io = socket_io(server);
 let queueServer = io.of('/player');
 
+
+let score;
 app.use(express.static(publicPath));
 
 io.on("connection", (socket) => {
@@ -20,12 +27,26 @@ io.on("connection", (socket) => {
   io.emit('showMessage')
 
   socket.on("startGame", (playerID) => {
+
     queueServer.emit('addPlayer', playerID);
+
     io.emit("startGame");
   });
-  
+
   socket.on("crazyClicked", (data) => {
-      io.emit('crazyClicked', data);
+    queueServer.emit('overWrite')
+    // data.score++;
+
+
+    // io.on('newScore', (score) => {
+    //   console.log(score.score, 'newwwwwwwwwwww')
+    // })
+
+    // console.log('score in crazy ', data.score)
+
+    queueServer.emit('updateScore', data.playerID)
+    io.emit('crazyClicked', data);
+    console.log(data, 'data in crazy')
   });
 
   socket.on("disconnect", () => {
@@ -34,11 +55,31 @@ io.on("connection", (socket) => {
 
 });
 
-queueServer.on('connection', () => {
-    console.log('queue is connected');
+queueServer.on('connection', socket => {
+  
+  console.log('queue is connected');
+  let bigScore;
+  
+  socket.on('newScore', score => {
+    console.log(score, 'score from q')
+    bigScore = score;
+    io.emit('newScore',bigScore)
+  })
+  socket.on('over2',(queue)=>{
+    console.log('overWrite on server')
+
+
+    io.emit('over',queue);
+
+  })
+  // socket.emit('addPlayer',payload)
 
 
 })
+
+
+
+
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
